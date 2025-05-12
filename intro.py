c666d3f19fdc9272c38f0501058e9fd4dace7225
unknown <anaba.vine@gmail.com>
2025-05-12 12:40:24 +0100

    Treated operaion

--- intro.py
+++ intro.py
@@ -106,6 +106,7 @@ print(y.rstrip()+"  "+fname)
 
 
 #count
+
 print(n.count('e'))
 h= f"i have the number {33}"
 print(h.count('3'))
@@ -137,7 +138,6 @@ print(yy[1])
 print(yy[1][2])
 print(yy[1],yy[2])
 
-
 #find
 print(js.find('bicycle'))
 
@@ -167,19 +167,169 @@ bx=11.345667788888888
 print(round(bx,3))
 
 #OPERATORS
-#arithmetric operator
-#comparism operator
-#assignment operator
-#bitwise operator '''
+#Arithmetric operator
+#Comparism operator
+#Assignment operator
+#Bitwise operator 
+#logical operator
+#Membership operator
+#Identity operator
 
 name= 'jack'
 text= 'Today is wednesday'
 
+#Comparison operators
 print(name=='paul') #Equals to
 print(name !='paul') #Not equals to
+print(2>5)
+
+
 print('is' in text) #membership
 
 num1,num2,num3= 10,12,10
 print(num1<num2)
 print(num1>num2)
+
+
+#Assignemnt operators
+b = "blessing"
+b += " "+ "henry"
+print(b)
+
+n = 20
+n += 5
+print(n)
+
+
+cal = 40
+cal -= 10
+print(cal)
+
+div = 33
+div /=3
+print(div)
+
+floor = 33
+floor //= 3
+print(floor)
+
+
+mod = 45
+mod %= 5
+print(mod)
+
+pow = 3
+pow **= 4
+print(pow)
+
+
+#Membership operators
+#in and not in
+l = {1,2,3,4,5}
+print(3 in l)
+print(10 in l)
+print(7 not in l)
+print(7 in l)
+
+
+#Logical operator
+# And, or
+print(11>5 and 12<10)
+print(2<3 and 20>10)
+
+print(11>4 or 10<30)
+print(9>5 or 20<10)
+print(3>23 or 20<9)
+
+#Comparison operator
+#equals to, not equal to
+print(2==3 or 3!=5)
+
+#Identity operators
+# Is, iS not
+
+print(10 is 4)
+print("henry" is "Henry")
+print('john' is not 'paul')
+
+#bitwise operator
+# | bitwise or
+# & bitwise and
+# ^ bitwise xor
+# ~ bitwise not
+# << bitwise left shift
+# >> bitwise right shift
+
+#Bitwise OR (|)
+#(returns 1 if at least one bit is 1, otherwise 0)
+
+a = 10
+b = 15
+
+# a = 1010
+# b = 1111
+# =1111
+
+print (a | b)
+
+
+c = 3
+d = 5
+# c = 011
+# d = 101
+#    =111
+
+print( c | d) 
+
+#Bitwise AND (&)
+#(Returns 1 if both bits are 1, else 0)
+
+c = 3
+d = 5
+# c = 011
+# d = 101
+#   = 001
+
+print( c & d)
+
+
+e = 7
+g =12
+print(e & g)
+
+# e =0111
+# g =1100
+#   =0100
+
+#bitwise XOR (^)
+#Returnd 1 if the bits are differnt, otherwise 0'''
+
+c = 3
+d = 5
+# c = 011
+# d = 101
+#   = 110
+
+print( c ^ d)
+
+#bitwise not (~)
+#Inverts all the bits (1 becomes 0, 0 becomes 1)
+
+print (~c)
+
+
+#bitwise right shift (>>)
+#Shifts bits to the left, gilling with zeros on the right. Effectively multiplies by the epower of a number
+
+print (d >> 2)
+
+#bitwise left shift (>>)
+#Shifts bits to the right, gilling with zeros on the right. Effectively multiplies by the epower of a number
+
+print (d << 2)
+
+
+
+
+
 
